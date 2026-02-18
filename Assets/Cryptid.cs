199e920001d7228d3f469e43dc97a8933d78e33d
Timwi <github@timwi.de>
2026-02-18 17:57:44 +0100

    * Simplify TP code (fixes bug where TILE shortcut didn’t work because the code said S instead of T * Add .editorconfig to silence the warning about unused members

--- Assets/Cryptid.cs
+++ Assets/Cryptid.cs
@@ -48,7 +48,7 @@ public class Cryptid : MonoBehaviour
     }
     void generatePuzzle()
     {
-    tryagain:
+        tryagain:
         Board board = new Board(ModuleName, ModuleId);
         //board = new Board("6CB395C6A6L9G3I7H5I3H3");
         if (spacesWithin == null)
@@ -85,7 +85,7 @@ public class Cryptid : MonoBehaviour
     private string getUnfollowedRules(string space)
     {
         string unfollowedRules = "";
-        for(int i = 0; i < rules.Length; i++)
+        for (int i = 0; i < rules.Length; i++)
         {
             if (!rules[i].validSpaces.Contains(space))
                 unfollowedRules += (i + 1);
@@ -151,7 +151,7 @@ public class Cryptid : MonoBehaviour
         displayCoordinate();
     }
 
-    
+
     int mod(int n, int m)
     {
         while (n < 0)
@@ -164,100 +164,76 @@ public class Cryptid : MonoBehaviour
     private readonly string TwitchHelpMessage = @"!{0} (C)ycle [ABCDEFGHIJKL123456789] will cycle through the coordinates that contains those letters/numbers. !{0} (T)ile F3 will go to the F3 space. !{0} (S)ubmit G7 will submit the G7 space. !{0} (S)ubmit will retrieve all spaces that are black.";
 #pragma warning restore 414
 
-    IEnumerator ProcessTwitchCommand(string command)
+    private IEnumerator ProcessTwitchCommand(string command)
     {
-        if(!TPautosolve)
+        if (TPautosolve)
         {
-            string[] param = command.ToUpper().Split(' ');
-            if ((Regex.IsMatch(param[0], @"^\s*CYCLE\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant) || Regex.IsMatch(param[0], @"^\s*C\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)) && param.Length == 2 && isValidCoordCycle(param[1]))
+            yield return "sendtochat Module is being solved at the moment.";
+            yield break;
+        }
+
+        Match m;
+        // (C)ycle [ABCDEFGHIJKL123456789]
+        if ((m = Regex.Match(command, @"^\s*c(?:ycle)?\s+([a-l1-9]+)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success)
+        {
+            yield return null;
+            var letters = m.Groups[1].Value.ToUpperInvariant().Where(c => "ABCDEFGHIJKL".Contains(c)).Distinct().ToArray();
+            var numbers = m.Groups[1].Value.Where(c => "123456789".Contains(c)).Distinct().ToArray();
+            if (letters.Length == 0 || numbers.Length == 0)
             {
-                yield return null;
-                string letters = "", numbers = "";
-                foreach(char cha in param[1])
+                yield return "sendtochat The cycle command requires at least one letter and at least one number.";
+                yield break;
+            }
+
+            foreach (var letter in letters)
+            {
+                yield return "trycancel Cycling has been cancelled due to a cancel request.";
+                while (letterText.text[0] != letter)
                 {
-                    if ("123456789".Contains(cha))
-                    {
-                        if(!numbers.Contains(cha))
-                            numbers += cha;
-                    }
-                    else
-                    {
-                        if (!letters.Contains(cha))
-                            letters += cha;
-                    } 
+                    Debug.Log($"[{letterText.text}] vs. [{letter}]");
+                    letterUp.OnInteract();
+                    yield return new WaitForSeconds(0.1f);
                 }
-                foreach(char letter in letters)
+                foreach (var number in numbers)
                 {
                     yield return "trycancel Cycling has been cancelled due to a cancel request.";
-                    while (letterText.text[0] != letter)
+                    while (numberText.text[0] != number)
                     {
-                        letterUp.OnInteract();
+                        numberUp.OnInteract();
                         yield return new WaitForSeconds(0.1f);
                     }
-                    foreach (char number in numbers)
-                    {
-                        yield return "trycancel Cycling has been cancelled due to a cancel request.";
-                        while (numberText.text[0] != number)
-                        {
-                            numberUp.OnInteract();
-                            yield return new WaitForSeconds(0.1f);
-                        }
-                        yield return new WaitForSeconds(1f);
-                    }
+                    yield return new WaitForSeconds(1f);
                 }
             }
-            else if ((Regex.IsMatch(param[0], @"^\s*TILE\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant) || Regex.IsMatch(param[0], @"^\s*S\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)) && param.Length == 2 && isValidSpace(param[1]))
+        }
+        // (T)ile / (S)ubmit [A-L][1-9]
+        else if ((m = Regex.Match(command, @"^\s*(?:t(?:ile)?|(?<submit>s(?:ubmit)?))\s+(?<c>[a-l])(?<r>[1-9])\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success)
+        {
+            yield return null;
+            while (letterText.text != m.Groups["c"].Value.ToUpperInvariant())
             {
-                yield return null;
-                while (letterText.text[0] != param[1][0])
-                {
-                    letterUp.OnInteract();
-                    yield return new WaitForSeconds(0.1f);
-                }
-                while (numberText.text[0] != param[1][1])
-                {
-                    numberUp.OnInteract();
-                    yield return new WaitForSeconds(0.1f);
-                }
+                letterUp.OnInteract();
+                yield return new WaitForSeconds(0.1f);
             }
-            else if ((Regex.IsMatch(param[0], @"^\s*SUBMIT\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant) || Regex.IsMatch(param[0], @"^\s*S\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)) && param.Length == 2 && isValidSpace(param[1]))
+            while (numberText.text != m.Groups["r"].Value)
             {
-                yield return null;
-                while (letterText.text[0] != param[1][0])
-                {
-                    letterUp.OnInteract();
-                    yield return new WaitForSeconds(0.1f);
-                }
-                while (numberText.text[0] != param[1][1])
-                {
-                    numberUp.OnInteract();
-                    yield return new WaitForSeconds(0.1f);
-                }
+                numberUp.OnInteract();
+                yield return new WaitForSeconds(0.1f);
+            }
+            if (m.Groups["submit"].Success)
                 submit.OnInteract();
-            }
-            else if ((Regex.IsMatch(param[0], @"^\s*SUBMIT\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant) || Regex.IsMatch(param[0], @"^\s*S\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)) && param.Length == 1)
-            {
-                yield return null;
-                string str = "";
-                foreach (string space in submitSpaces)
-                    str += space + " ";
-                yield return "sendtochat " + str;
-            }
-            else
-                yield return "sendtochat An error occured because the user inputted something wrong.";
+        }
+        // (S)ubmit (no coordinate)
+        else if ((m = Regex.Match(command, @"^\s*s(?:ubmit)?\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success)
+        {
+            yield return null;
+            yield return $"sendtochat {submitSpaces.Join(" ")}";
         }
         else
-            yield return "sendtochat Module is being solved at the moment.";
-        yield return null;
-    }
-    bool isValidSpace(string input)
-    {
-        if(input.Length == 2)
-            return "ABCDEFGHIJKL".Contains(input[0]) && "123456789".Contains(input[1]);
-        return false;
+            yield return "sendtochat An error occured because the user inputted something wrong.";
     }
 
-    IEnumerator TwitchHandleForcedSolve()
+    private IEnumerator TwitchHandleForcedSolve()
     {
         TPautosolve = true;
         yield return null;
@@ -272,19 +248,5 @@ public class Cryptid : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
         submit.OnInteract();
-    }
-    bool isValidCoordCycle(string str)
-    {
-        bool b1 = false, b2 = false;
-        foreach(char cha in str)
-        {
-            if (!"ABCDEFGHIJKL123456789".Contains(cha))
-                return false;
-            else if ("123456789".Contains(cha))
-                b1 = true;
-            else
-                b2 = true;
-        }
-        return b1 && b2;
     }
 }
